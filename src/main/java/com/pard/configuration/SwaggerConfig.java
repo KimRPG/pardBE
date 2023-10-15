250bff9608544871f27d9f341233174823c67f89
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-10-15 16:48:52 +0900

    swagger 완성

--- src/main/java/com/pard/configuration/SwaggerConfig.java
+++ src/main/java/com/pard/configuration/SwaggerConfig.java
@@ -18,8 +18,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
-                .version("1.0.0");
+                .title("Springdoc 테스트") //제목
+                .description("Springdoc을 사용한 Swagger UI 테스트") // 설명
+                .version("1.0.0"); // version
     }
 }
